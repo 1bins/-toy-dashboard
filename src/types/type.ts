9bf4b15d411ca6d38f22912e0a9a8c4cb8da5f4c
서원빈 <1bins@kakao.com>
2025-07-17 16:33:52 +0900

    :sparkles: FlexProps 타입 정의

---
+++ src/types/type.ts
@@ -0,0 +1,6 @@
+export interface FlexProps {
+  direction?: 'col' | 'row';
+  align?: 'center' | 'start' | 'end';
+  justify?: 'start' | 'center' | 'end' | 'between';
+  wrap?: 'wrap' | 'nowrap';
+}
